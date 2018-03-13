6fccb479a35ba0eac41e472f35ef015f9c709f29
Gary <Gking19@users.noreply.github.com>
2018-03-12 18:49:14 -0700

    First version

---
+++ InterestCalculator/InterestCalculator/Form1.Designer.vb
@@ -0,0 +1,61 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.lstOutput = New System.Windows.Forms.ListBox()
+        Me.btnCalculate = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'lstOutput
+        '
+        Me.lstOutput.FormattingEnabled = True
+        Me.lstOutput.Location = New System.Drawing.Point(12, 12)
+        Me.lstOutput.Name = "lstOutput"
+        Me.lstOutput.Size = New System.Drawing.Size(260, 342)
+        Me.lstOutput.TabIndex = 0
+        '
+        'btnCalculate
+        '
+        Me.btnCalculate.Location = New System.Drawing.Point(105, 361)
+        Me.btnCalculate.Name = "btnCalculate"
+        Me.btnCalculate.Size = New System.Drawing.Size(75, 23)
+        Me.btnCalculate.TabIndex = 1
+        Me.btnCalculate.Text = "Calculate Interest"
+        Me.btnCalculate.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(284, 519)
+        Me.Controls.Add(Me.btnCalculate)
+        Me.Controls.Add(Me.lstOutput)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents lstOutput As System.Windows.Forms.ListBox
+    Friend WithEvents btnCalculate As System.Windows.Forms.Button
+
+End Class
---
+++ InterestCalculator/InterestCalculator/Form1.vb
@@ -0,0 +1,39 @@
+﻿Public Class Form1
+
+    Private Sub btnCalculate_Click(sender As Object, e As EventArgs) Handles btnCalculate.Click
+
+        Dim Year As Integer
+        Dim TotalSimple As Double
+        Dim TotalCompound As Double
+        Dim SimpleBeginning As Double
+        Dim CompoundBeginning As Double
+        Dim SimpleRunningTotal As Double
+        Dim CompoundRunningTotal As Double
+        Dim InterestRate As Double
+
+        Dim fmtstr As String = "{0,-6} {1,12} {2,12}" 'Use for formating output.
+        lstOutput.Items.Clear()
+        lstOutput.Items.Add("")
+        lstOutput.Items.Add(String.Format(fmtstr, "", "Amount", "Amount"))
+
+        SimpleBeginning = InputBox("Beginning simple interest amount", "Enter beginning amount: ")
+        CompoundBeginning = InputBox("Beginning compound interest amount", "Enter beginning amount: ")
+        InterestRate = InputBox("Interest Rate", "Enter interest rate: ")
+        Year = InputBox("Periods in years", "Enter amount of years: ")
+
+        For J = 1 To Year
+
+            SimpleRunningTotal += SimpleBeginning * ((1 + InterestRate) * Year)
+            TotalSimple = SimpleRunningTotal
+
+
+
+
+            lstOutput.Items.Add(String.Format(fmtstr, Year, TotalSimple, TotalCompound))
+
+            Year += 1
+        Next
+
+
+    End Sub
+End Class
