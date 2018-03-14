37593e122a375c33b3ad32f0dd4b12b18e57f496
Gary <Gking19@users.noreply.github.com>
2018-03-14 15:45:26 -0700

    Fixed simple interest, added compound
    Simple interest works, compound interest not working.

--- InterestCalculator/InterestCalculator/Form1.vb
+++ InterestCalculator/InterestCalculator/Form1.vb
@@ -7,9 +7,9 @@
         Dim TotalCompound As Double
         Dim SimpleBeginning As Double
         Dim CompoundBeginning As Double
-        Dim SimpleRunningTotal As Double
         Dim CompoundRunningTotal As Double
         Dim InterestRate As Double
+        Dim TimesCompounded As Integer
 
         Dim fmtstr As String = "{0,-6} {1,12} {2,12}" 'Use for formating output.
         lstOutput.Items.Clear()
@@ -18,20 +18,17 @@
 
         SimpleBeginning = InputBox("Beginning simple interest amount", "Enter beginning amount: ")
         CompoundBeginning = InputBox("Beginning compound interest amount", "Enter beginning amount: ")
+        TimesCompounded = InputBox("Amount of times compounded", "Enter amount of times to compound: ")
         InterestRate = InputBox("Interest Rate", "Enter interest rate: ")
         Year = InputBox("Periods in years", "Enter amount of years: ")
-
+        Dim ConvertedInterest As Double = InterestRate / 100
         For J = 1 To Year
 
-            SimpleRunningTotal += SimpleBeginning * ((1 + InterestRate) * Year)
-            TotalSimple = SimpleRunningTotal
+            TotalSimple = SimpleBeginning + (SimpleBeginning * ConvertedInterest * J)
+            CompoundRunningTotal = CompoundBeginning
+            TotalCompound = CompoundRunningTotal + (CompoundBeginning * (CompoundRunningTotal * ConvertedInterest * J))
+            lstOutput.Items.Add(String.Format(fmtstr, J, TotalSimple, TotalCompound))
 
-
-
-
-            lstOutput.Items.Add(String.Format(fmtstr, Year, TotalSimple, TotalCompound))
-
-            Year += 1
         Next
 
 
