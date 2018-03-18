ee320cc9ca3bfe8fd2c6434f3a73c09c6b3d9f7e
Gary <Gking19@users.noreply.github.com>
2018-03-18 14:13:08 -0700

    5

--- InterestCalculator/InterestCalculator/Form1.vb
+++ InterestCalculator/InterestCalculator/Form1.vb
@@ -6,28 +6,34 @@
         Dim TotalSimple As Double
         Dim TotalCompound As Double
         Dim SimpleBeginning As Double
+        Dim SimpleRunningTotal As Double
         Dim CompoundBeginning As Double
         Dim CompoundRunningTotal As Double
         Dim InterestRate As Double
         Dim TimesCompounded As Integer
 
-        Dim fmtstr As String = "{0,-6} {1,12} {2,12}" 'Use for formating output.
+        Dim fmtstr As String = "{0,-6} {1,12:C2} {2,12:C2}" 'Use for formating output.
         lstOutput.Items.Clear()
         lstOutput.Items.Add("")
         lstOutput.Items.Add(String.Format(fmtstr, "", "Amount", "Amount"))
 
         SimpleBeginning = InputBox("Beginning simple interest amount", "Enter beginning amount: ")
         CompoundBeginning = InputBox("Beginning compound interest amount", "Enter beginning amount: ")
-        TimesCompounded = InputBox("Amount of times compounded", "Enter amount of times to compound: ")
-        InterestRate = InputBox("Interest Rate", "Enter interest rate: ")
+        TimesCompounded = InputBox("Amount of times compounded per year", "Enter amount of times to compound: ")
+        InterestRate = InputBox("Interest Rate as percent", "Enter interest rate: ")
         Year = InputBox("Periods in years", "Enter amount of years: ")
-        Dim ConvertedInterest As Double = InterestRate / 100
+        Dim ConvertedCompoundInterest As Double = (InterestRate / 100) + 1
+        Dim ConvertedInterest As Double = (InterestRate / 100)
+        SimpleRunningTotal = SimpleBeginning
+        CompoundRunningTotal = CompoundBeginning
         For J = 1 To Year
 
-            TotalSimple = SimpleBeginning + (SimpleBeginning * ConvertedInterest * J)
-            CompoundRunningTotal = CompoundBeginning
-            TotalCompound = CompoundRunningTotal + (CompoundBeginning * (CompoundRunningTotal * ConvertedInterest * J))
+            TotalSimple = SimpleRunningTotal + (SimpleBeginning * ConvertedInterest)
+            'TotalCompound = (CompoundRunningTotal * (1 + ConvertedInterest)) 'compound interest?
+            TotalCompound = (CompoundRunningTotal * (((1 + ConvertedInterest)) / TimesCompounded) ^ J)
             lstOutput.Items.Add(String.Format(fmtstr, J, TotalSimple, TotalCompound))
+            SimpleRunningTotal = TotalSimple
+            CompoundRunningTotal = TotalCompound
 
         Next
 
